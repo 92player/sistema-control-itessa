4cd6aa16dd2003454727206c0e1eb2a234b7868a
92player <aldojasm@gmail.com>
2022-09-27 15:51:00 -0500

    generar orden

--- src/upc/eda/siscon/Inventario.java
+++ src/upc/eda/siscon/Inventario.java
@@ -1,5 +1,6 @@
 package upc.eda.siscon;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Inventario {
@@ -12,14 +13,20 @@ public class Inventario {
 		boolean condicionMenu = true;
 
 		/* Datos del Producto */
-		int producto = 0;
-		String nombreProducto[] = new String[producto];
-		String tipoProducto[] = new String[producto];
-		String pesoProducto[] = new String[producto];
-		String anchuraProducto[] = new String[producto];
-		String composicionProducto[] = new String[producto];
-		String unidadProducto[] = new String[producto];
-		String colorProducto[] = new String[producto];
+		LinkedList nombreProducto = new LinkedList();
+		LinkedList tipoProducto = new LinkedList();
+		LinkedList pesoProducto = new LinkedList();
+		LinkedList anchuraProducto = new LinkedList();
+		LinkedList composicionProducto = new LinkedList();
+		LinkedList unidadProducto = new LinkedList();
+		LinkedList colorProducto = new LinkedList();		
+		LinkedList stockProducto = new LinkedList();
+		
+		/* Datos de Solicitud de Productos */
+		LinkedList solicitud = new LinkedList();
+		
+		/* Datos de Orden de Compra */
+		LinkedList ordenCompra = new LinkedList();
 
 		while (condicionMenu) {
 			System.out.println(" -----------------");
@@ -42,34 +49,28 @@ public class Inventario {
 				System.out.println("[2]Productos");
 				System.out.print("Cantidad de productos a ingresar: ");
 
-				producto = opcionSeleccionMenu.nextInt();
+				int producto = opcionSeleccionMenu.nextInt();
 				opcionSeleccionMenu.nextLine();
-
-				nombreProducto = new String[producto];
-				tipoProducto = new String[producto];
-				pesoProducto = new String[producto];
-				anchuraProducto = new String[producto];
-				composicionProducto = new String[producto];
-				unidadProducto = new String[producto];
-				colorProducto = new String[producto];
 
 				for (int i = 0; i < producto; i++) {
 
 					System.out.println("-> Producto " + (i + 1));
 					System.out.print("Nombre: ");
-					nombreProducto[i] = opcionSeleccionMenu.next();
+					nombreProducto.add(opcionSeleccionMenu.next());
 					System.out.print("Tipo: ");
-					tipoProducto[i] = opcionSeleccionMenu.next();
+					tipoProducto.add(opcionSeleccionMenu.next());
 					System.out.print("Peso: ");
-					pesoProducto[i] = opcionSeleccionMenu.next();
+					pesoProducto.add(opcionSeleccionMenu.next());
 					System.out.print("Anchura: ");
-					anchuraProducto[i] = opcionSeleccionMenu.next();
+					anchuraProducto.add(opcionSeleccionMenu.next());
 					System.out.print("Composicion: ");
-					composicionProducto[i] = opcionSeleccionMenu.next();
+					composicionProducto.add(opcionSeleccionMenu.next());
 					System.out.print("Unidad: ");
-					unidadProducto[i] = opcionSeleccionMenu.next();
+					unidadProducto.add(opcionSeleccionMenu.next());
 					System.out.print("Color: ");
-					colorProducto[i] = opcionSeleccionMenu.next();
+					colorProducto.add(opcionSeleccionMenu.next());
+					System.out.print("Stock: ");
+					stockProducto.add(opcionSeleccionMenu.nextInt());
 
 				}
 
@@ -79,21 +80,56 @@ public class Inventario {
 				System.out.println("[3]Stock");
 				System.out.println("Cantidad de productos disponibles");
 
-				for (int i = 0; i < producto; i++) {
+				for (int i = 0; i < nombreProducto.size(); i++) {
 					System.out.println("-> Producto " + (i + 1));
-					System.out.println("Nombre: " + nombreProducto[i]);
-					System.out.println("Tipo: " + tipoProducto[i]);
-					System.out.println("Peso: " + pesoProducto[i]);
-					System.out.println("Anchura: " + anchuraProducto[i]);
-					System.out.println("Composicion: " + composicionProducto[i]);
-					System.out.println("Unidad: " + unidadProducto[i]);
-					System.out.println("Color: " + colorProducto);
+					System.out.println("Nombre: " + nombreProducto.get(i));
+					System.out.println("Tipo: " + tipoProducto.get(i));
+					System.out.println("Peso: " + pesoProducto.get(i));
+					System.out.println("Anchura: " + anchuraProducto.get(i));
+					System.out.println("Composicion: " + composicionProducto.get(i));
+					System.out.println("Unidad: " + unidadProducto.get(i));
+					System.out.println("Color: " + colorProducto.get(i));
+					System.out.println("Stock: " + stockProducto.get(i));
 				}
 
 				break;
 
 			case 4:
 				System.out.println("[4]Generar Orden");
+				System.out.print("Nombre de producto: ");
+				String nombre = opcionSeleccionMenu.next();
+				
+				if(nombreProducto.size() == 0) {
+					System.out.println("No se tiene registrado ningun producto.");
+				}
+				
+				boolean existe = false;
+				
+				for (int i = 0; i < nombreProducto.size(); i++) {
+					if(nombreProducto.get(i).equals(nombre)) {
+						
+						System.out.print("Indicar cantidad de producto: ");
+						int cantidadSolicitada = opcionSeleccionMenu.nextInt();
+						int stockActual = Integer.parseInt(stockProducto.get(i).toString());
+						
+						if(cantidadSolicitada > stockActual) {
+							System.out.println("No hay stock del producto solicitado. Se ha generado una OC.");
+							ordenCompra.add("Nombre producto: "+nombreProducto.get(i)+" - Cantidad: "+cantidadSolicitada);
+						} else {
+							solicitud.add("Nombre producto: "+nombreProducto.get(i)+" - Cantidad: "+cantidadSolicitada);
+							stockProducto.set(i, stockActual - cantidadSolicitada);
+							System.out.println("Solicitud ha sido realizada con exito.");
+						}
+						existe = true;
+					}
+				}
+				
+				if(!existe){
+					System.out.println("Producto no encontrado.");					
+				} else {
+					existe = false;
+				}
+				
 				break;
 
 			case 5:
