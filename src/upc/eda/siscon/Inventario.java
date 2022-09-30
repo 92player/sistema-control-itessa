8e0d834873bc9857258893e13272605e7c8536cb
92player <aldojasm@gmail.com>
2022-09-30 01:07:10 -0500

    update dummys

--- src/upc/eda/siscon/Inventario.java
+++ src/upc/eda/siscon/Inventario.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 
 public class Inventario {
 
+	/* Datos del Producto */
+	static LinkedList nombreProducto = new LinkedList();
+	static LinkedList tipoProducto = new LinkedList();
+	static LinkedList pesoProducto = new LinkedList();
+	static LinkedList anchuraProducto = new LinkedList();
+	static LinkedList composicionProducto = new LinkedList();
+	static LinkedList colorProducto = new LinkedList();
+	static LinkedList stockProducto = new LinkedList();
+
 	public static void main(String[] args) {
 
 		Scanner opcionSeleccionMenu = new Scanner(System.in);
@@ -12,19 +21,11 @@ public class Inventario {
 		int opcionMenu = 0;
 		boolean condicionMenu = true;
 
-		/* Datos del Producto */
-		LinkedList nombreProducto = new LinkedList();
-		LinkedList tipoProducto = new LinkedList();
-		LinkedList pesoProducto = new LinkedList();
-		LinkedList anchuraProducto = new LinkedList();
-		LinkedList composicionProducto = new LinkedList();
-		LinkedList unidadProducto = new LinkedList();
-		LinkedList colorProducto = new LinkedList();		
-		LinkedList stockProducto = new LinkedList();
-		
+		agregarDummys();
+
 		/* Datos de Solicitud de Productos */
 		LinkedList solicitud = new LinkedList();
-		
+
 		/* Datos de Orden de Compra */
 		LinkedList ordenCompra = new LinkedList();
 
@@ -43,6 +44,7 @@ public class Inventario {
 			switch (opcionMenu) {
 			case 1:
 				System.out.println("[1]Menu");
+				System.out.println("Retornando al menu principal");
 				break;
 
 			case 2:
@@ -65,8 +67,6 @@ public class Inventario {
 					anchuraProducto.add(opcionSeleccionMenu.next());
 					System.out.print("Composicion: ");
 					composicionProducto.add(opcionSeleccionMenu.next());
-					System.out.print("Unidad: ");
-					unidadProducto.add(opcionSeleccionMenu.next());
 					System.out.print("Color: ");
 					colorProducto.add(opcionSeleccionMenu.next());
 					System.out.print("Stock: ");
@@ -82,14 +82,13 @@ public class Inventario {
 
 				for (int i = 0; i < nombreProducto.size(); i++) {
 					System.out.println("-> Producto " + (i + 1));
-					System.out.println("Nombre: " + nombreProducto.get(i));
-					System.out.println("Tipo: " + tipoProducto.get(i));
-					System.out.println("Peso: " + pesoProducto.get(i));
-					System.out.println("Anchura: " + anchuraProducto.get(i));
-					System.out.println("Composicion: " + composicionProducto.get(i));
-					System.out.println("Unidad: " + unidadProducto.get(i));
-					System.out.println("Color: " + colorProducto.get(i));
-					System.out.println("Stock: " + stockProducto.get(i));
+					System.out.println("\t Nombre \t: " + nombreProducto.get(i));
+					System.out.println("\t Tipo \t\t: " + tipoProducto.get(i));
+					System.out.println("\t Peso \t\t: " + pesoProducto.get(i));
+					System.out.println("\t Anchura \t: " + anchuraProducto.get(i));
+					System.out.println("\t Composicion \t: " + composicionProducto.get(i));
+					System.out.println("\t Color \t\t: " + colorProducto.get(i));
+					System.out.println("\t Stock \t\t: " + stockProducto.get(i));
 				}
 
 				break;
@@ -98,42 +97,48 @@ public class Inventario {
 				System.out.println("[4]Generar Orden");
 				System.out.print("Nombre de producto: ");
 				String nombre = opcionSeleccionMenu.next();
-				
-				if(nombreProducto.size() == 0) {
+
+				// Validar si existen productos registrados
+				if (nombreProducto.size() == 0) {
 					System.out.println("No se tiene registrado ningun producto.");
 				}
-				
-				boolean existe = false;
-				
+
+				int cantidadEncontrada = 0;
+
 				for (int i = 0; i < nombreProducto.size(); i++) {
-					if(nombreProducto.get(i).equals(nombre)) {
-						
+					if (nombreProducto.get(i).equals(nombre)) {
+
 						System.out.print("Indicar cantidad de producto: ");
 						int cantidadSolicitada = opcionSeleccionMenu.nextInt();
 						int stockActual = Integer.parseInt(stockProducto.get(i).toString());
-						
-						if(cantidadSolicitada > stockActual) {
+
+						if (cantidadSolicitada > stockActual) {
 							System.out.println("No hay stock del producto solicitado. Se ha generado una OC.");
-							ordenCompra.add("Nombre producto: "+nombreProducto.get(i)+" - Cantidad: "+cantidadSolicitada);
+							ordenCompra.add(
+									"Nombre producto: " + nombreProducto.get(i) + " - Cantidad: " + cantidadSolicitada);
 						} else {
-							solicitud.add("Nombre producto: "+nombreProducto.get(i)+" - Cantidad: "+cantidadSolicitada);
+							solicitud.add(
+									"Nombre producto: " + nombreProducto.get(i) + " - Cantidad: " + cantidadSolicitada);
 							stockProducto.set(i, stockActual - cantidadSolicitada);
 							System.out.println("Solicitud ha sido realizada con exito.");
 						}
-						existe = true;
+						cantidadEncontrada++;
+
 					}
 				}
-				
-				if(!existe){
-					System.out.println("Producto no encontrado.");					
-				} else {
-					existe = false;
+
+				if (cantidadEncontrada == 0) {
+					System.out.println("No se tiene registrado producto con ese nombre.");
 				}
-				
+
 				break;
 
 			case 5:
 				System.out.println("[5]Info");
+				System.out.println("*************** Integrantes Grupo 03 ***************");
+				System.out.println("- Francisco Javier Espinoza");
+				System.out.println("- Jhordan Soria Miranda");
+				System.out.println("- Enrique Becerra");
 				break;
 
 			case 0:
@@ -160,6 +165,28 @@ public class Inventario {
 			}
 
 		}
+	}
+
+	static void agregarDummys() {
+
+		// Producto 01
+		nombreProducto.add("Algodon");
+		tipoProducto.add("Natural");
+		pesoProducto.add("120g/m2");
+		anchuraProducto.add("145cm");
+		composicionProducto.add("100%Algodón");
+		colorProducto.add("Blanco");
+		stockProducto.add("20");
+
+		// Producto 02
+		nombreProducto.add("Seda");
+		tipoProducto.add("Natural");
+		pesoProducto.add("150g/m2");
+		anchuraProducto.add("145cm");
+		composicionProducto.add("100%Poliester");
+		colorProducto.add("Verde");
+		stockProducto.add("10");
+
 	}
 
 }
